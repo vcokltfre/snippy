25d382f3b220d3040569dc1aaaefd57a160c7d16
vcokltfre <vcokltfre@gmail.com>
2023-02-07 23:17:50 +0000

    feat: setup go project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vcokltfre/snippy
+
+go 1.19
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/joho/godotenv"
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {}
