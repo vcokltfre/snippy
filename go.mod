5b867f8ce12e2922b2f24aab955140860b16a07f
vcokltfre <vcokltfre@gmail.com>
2023-02-07 23:20:22 +0000

    feat: setup cli

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/vcokltfre/snippy
 
 go 1.19
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/urfave/cli/v2 v2.24.3
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/urfave/cli/v2"
+)
+
+var app = &cli.App{
+	Name:     "snippy",
+	Usage:    "A simple way to store and manage snippets of code",
+	Commands: []*cli.Command{},
+}
 
 func init() {
 	godotenv.Load()
 }
 
-func main() {}
+func main() {
+	err := app.Run(os.Args)
+	if err != nil {
+		if os.Getenv("DEBUG") == "true" {
+			panic(err)
+		}
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+}
